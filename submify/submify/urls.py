8e10a62052f252c21c3898f70fc10d23c7261af0
Anjansrivathsav <anjansrivathsav1997@gmail.com>
2017-11-11 00:13:22 +0530

    Update urls.py

--- submify/submify/urls.py
+++ submify/submify/urls.py
@@ -2,7 +2,7 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
+Examples::
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
